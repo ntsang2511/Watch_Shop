997dc55a08b6bfbbb670448a1c8657c6319ea9ad
Dương Nguyễn Võ Lê <duongnvl21@uef.edu.vn>
2024-06-20 23:31:32 +0700

    wsContext, database

---
+++ Web_Watch_Shop/Data/Watch_ShopContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Web_Watch_Shop.Models;
+
+namespace Web_Watch_Shop.Data
+{
+    public class Watch_ShopContext : DbContext
+    {
+        public Watch_ShopContext (DbContextOptions<Watch_ShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Web_Watch_Shop.Models.Product> Product { get; set; } = default!;
+    }
+}
