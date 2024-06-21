4b71aeb30144139d56829355ebcf7cfed93b8820
Dương Nguyễn Võ Lê <duongnvl21@uef.edu.vn>
2024-06-21 16:35:15 +0700

    Admin Page

---
+++ Web_Watch_Shop/Pages/Admin/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Web_Watch_Shop.Data;
+using Web_Watch_Shop.Models;
+
+namespace Web_Watch_Shop.Pages.Admin
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Web_Watch_Shop.Data.Web_Watch_ShopContext _context;
+
+        public DetailsModel(Web_Watch_Shop.Data.Web_Watch_ShopContext context)
+        {
+            _context = context;
+        }
+
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FirstOrDefaultAsync(m => m.ProductID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}
