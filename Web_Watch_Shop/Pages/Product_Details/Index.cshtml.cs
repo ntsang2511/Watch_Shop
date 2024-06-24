0c83c181615a85fe4b9332fe3dee17a9cb668aee
ntsang2511 <tansang25112003@gmail.com>
2024-06-24 23:03:54 +0700

    Add search by name and by category for admin, change validation for product detail model

--- Web_Watch_Shop/Pages/Product_Details/Index.cshtml.cs
+++ Web_Watch_Shop/Pages/Product_Details/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Web_Watch_Shop.Data;
 using Web_Watch_Shop.Models;
@@ -21,9 +22,34 @@ namespace Web_Watch_Shop.Pages.Product_Details
 
         public IList<Product_Detail> Product_Detail { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Categories { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? ProductCategory { get; set; }
         public async Task OnGetAsync()
         {
-            Product_Detail = await _context.Product_Detail.ToListAsync();
+            //Product_Detail = await _context.Product_Detail.ToListAsync();
+            IQueryable<string> categoryQuery = from m in _context.Product_Detail
+                                            orderby m.CategoryName
+                                            select m.CategoryName;
+
+            var products = from m in _context.Product_Detail
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                products = products.Where(s => s.ProductName.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(ProductCategory))
+            {
+                products = products.Where(x => x.CategoryName == ProductCategory);
+            }
+            Categories = new SelectList(await categoryQuery.Distinct().ToListAsync());
+            Product_Detail = await products.ToListAsync();
         }
     }
 }
